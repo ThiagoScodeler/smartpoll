baebe863909f1314e35fe097067c189db2c0cba2
rafaelwsilva <rws1602@gmail.com>
2014-05-17 17:14:43 -0300

    Update 2 - Dashboard

---
+++ dashboard/js/polls.js
@@ -0,0 +1,149 @@
+$(function () 
+{
+    //-----------------------------------------------------------------------
+    // 2) Send a http request with AJAX http://api.jquery.com/jQuery.ajax/
+    //-----------------------------------------------------------------------
+    $.ajax({                                      
+      url: 'includes/getPolls.php',    //the script to call to get data          
+      data: '',                        //you can insert url argumnets here to pass to getPolls.php
+                                       //for example "id=5&parent=6"
+      dataType: 'json',                //data format      
+      success: function(json)          //on recieve of reply
+      {
+
+        if (json.length>0) {
+
+        //--------------------------------------------------------------------
+        // 3) Update html content
+        //--------------------------------------------------------------------
+        
+          //accordion
+          accordion= $('<div>',{
+            'class' : 'panel-group',
+            'id' : 'accordion'
+          }).appendTo($('#output'));
+
+          //panel
+          panel= $('<div>',{
+            'class' : 'panel panel-info'
+          }).appendTo($(accordion));
+
+          //panel-heading
+          linkTitle = $('<h4>',{
+            'class' : 'panel-title',
+            'html' : $('<a>',{
+              'data-toggle' : 'collapse',
+              'data-parent' : '#accordion',
+              'href' : '#collapse'+0,
+              'text' : json[0].title
+            })
+          });
+
+          pHeading=document.createElement('div');
+          $(pHeading).addClass('panel-heading') 
+          .html($(linkTitle))
+          .appendTo($(panel));
+
+
+          //panel-body
+          divCollapseBody = $('<div>',{
+            'id' : 'collapse'+0,
+            'class' : 'panel-collapse collapse in',
+          }).appendTo($(panel));
+
+
+          pBody=document.createElement('div'); 
+          $(pBody).addClass('panel-body')
+          .html(json[0].question)
+          .appendTo($(divCollapseBody));
+
+          //buttons
+          divBtn=$('<div>',{
+            'class' : 'group-btn'
+          }).appendTo($(pBody));
+
+          btnEdit = ($('<button>', {
+            'type' : 'button', 
+            'class' : 'btn btn-warning btn-xs btnEdit',
+            'data-toggle' : 'modal',
+            'data-target' : '#myModal',
+            'html' : '<span class="glyphicon glyphicon-edit"></span> Edit'}).button()
+          );
+          btnEdit.appendTo($(divBtn));
+
+          btnRemove = ($('<button>', {
+            'type': 'button', 
+            'class': 'btn btn-danger btn-xs btnRemove', 
+            'data-toggle' : 'modal',
+            'data-target' : '#modalRemove',
+            'html': '<span class="glyphicon glyphicon-remove"></span> Remove'}).button()
+          );
+          btnRemove.appendTo($(divBtn));
+
+          //Repet panels
+          for (var i = 1; i < json.length; i++) {
+              //panel
+              panel= $('<div>',{
+                'class' : 'panel panel-info'
+              }).appendTo($(accordion));
+
+
+               //panel-heading
+               linkTitle = $('<h4>',{
+                'class' : 'panel-title',
+                'html' : $('<a>',{
+                  'data-toggle' : 'collapse',
+                  'data-parent' : '#accordion',
+                  'href' : '#collapse'+i,
+                  'text' : json[i].title
+                })
+              });
+
+               pHeading=document.createElement('div');
+               $(pHeading).addClass('panel-heading') 
+               .html($(linkTitle))
+               .appendTo($(panel));
+
+
+              //panel-body
+              divCollapseBody = $('<div>',{
+                'id' : 'collapse'+i,
+                'class' : 'panel-collapse collapse',
+              }).appendTo($(panel));
+              
+
+              pBody=document.createElement('div'); 
+              $(pBody).addClass('panel-body')
+              .html(json[i].question)
+              .appendTo($(divCollapseBody));
+
+              //buttons
+              divBtn=$('<div>',{
+                'class' : 'group-btn'
+              }).appendTo($(pBody));
+
+              btnEdit = ($('<button>', {
+                'type' : 'button', 
+                'class' : 'btn btn-warning btn-xs btnEdit',
+                'data-toggle' : 'modal',
+                'data-target' : '#myModal',
+                'html' : '<span class="glyphicon glyphicon-edit"></span> Edit'}).button()
+              );
+              btnEdit.appendTo($(divBtn));
+
+              btnRemove = ($('<button>', {
+                'type': 'button', 
+                'class': 'btn btn-danger btn-xs btnRemove', 
+                'data-toggle' : 'modal',
+                'data-target' : '#modalRemove',
+                'html': '<span class="glyphicon glyphicon-remove"></span> Remove'}).button()
+              );
+              btnRemove.appendTo($(divBtn));
+
+            };
+};        
+        //recommend reading up on jquery selectors they are awesome 
+        // http://api.jquery.com/category/selectors/
+      } 
+    });
+}); 
